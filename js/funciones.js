75591efc57ae7f8f571f0db32fbc5091bef645e5
sonya <sonyasanchezsoto@gmail.com>
2021-07-29 22:45:09 -0500

    operadores

--- js/funciones.js
+++ js/funciones.js
@@ -15,3 +15,24 @@ function MiTercerFuncion (a,b){
     return mul - 2;
 };
 console.log(MiTercerFuncion(2,3));
+
+// Funciones de tipo Expresión
+
+var tipoDos  = function(){
+    return 12;
+}
+
+console.log(tipoDos());
+
+// Template Literals
+function miNombre(nom){
+    var nombre = `${nom}`;
+    return nombre; 
+}
+console.log(miNombre('Sonia'));
+
+// Expresión de Función
+var saludar = function(nombre){
+    console.log(`Hola persona llamada: ${nombre}`);
+}
+console.log(saludar('Sanchez'));
---
+++ js/operadores.js
@@ -0,0 +1,27 @@
+// Operadores
+
+4 + 5 //Suma
+8 - 2 //Resta
+10 * 10 //Multiplicación
+9 / 3 // División
+
+'Sonia' + 'Sanchez'
+
+//Operdoares Unitarios
+
+!false //negación de la negación
+
+//Operador de asignación
+var uno = 1; 
+
+//Operación de igualdas
+
+3 == '3'
+
+3 === '3'
+
+5 < 3
+5 > 3
+5 <=3
+5 =< 3
+
